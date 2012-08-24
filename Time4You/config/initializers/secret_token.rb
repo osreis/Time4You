f98cf88ae8902bb157b20f1755b5ee7652c92fe8
Caio <caiotuxo@gmail.com>
2012-08-24 09:12:01 -0300

    inicio

---
+++ Time4You/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Time4You::Application.config.secret_token = 'f1c7566e738c529d0f71214d061ad514ce94555c1c5c9bc274b892a74abcec8f29cef5649b36c084938e58b6ce5075d42cdd7dd6bec7c134fbc186e4f781eaad'
---
+++ Time4You/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Time4You::Application.config.session_store :cookie_store, key: '_Time4You_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Time4You::Application.config.session_store :active_record_store
