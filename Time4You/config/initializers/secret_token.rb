bd4de1cbf83976f08395e114550a3f7bef4702d3
Raphael Sampaio <raphaelsampaio@gmail.com>
2012-08-24 17:31:28 -0300

    inicio

---
+++ Time4You/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Time4You::Application.config.secret_token = 'dbf78a1368608fcf6efea0b6a3074ffe38fa120f2c39ebee0a8976904982b38fea9dfd71a8e5f603c9ea53e0f8e580655ea250b6b67e038252ef378bc1c3b634'
