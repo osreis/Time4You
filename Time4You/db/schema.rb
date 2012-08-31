4626ba020c08f3f8675d7e97c6c56af789504b8f
Raphael Sampaio <raphaelsampaio@gmail.com>
2012-08-31 17:35:48 -0300

    marca incompleto

--- Time4You/db/schema.rb
+++ Time4You/db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120830203752) do
+ActiveRecord::Schema.define(:version => 20120831180724) do
+
+  create_table "brands", :force => true do |t|
+    t.string   "name"
+    t.string   "representative_name"
+    t.string   "representative_email"
+    t.string   "representative_phone"
+    t.text     "extra_info"
+    t.datetime "created_at",           :null => false
+    t.datetime "updated_at",           :null => false
+  end
 
   create_table "groups", :force => true do |t|
     t.string   "name"
